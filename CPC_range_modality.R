2a3b58e27e48bf256fe46be74c15548cccdfe569
Luciana de Macedo <lucianadiasdemacedo@gmail.com>
2025-09-18 12:15:56 -0400

    script with ranges separated

--- CPC_range_modality.R
+++ CPC_range_modality.R
@@ -3,18 +3,36 @@ rm (list = ls())
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(purrr)
 
 data <- read_excel("~/Downloads/CPC_2023.xlsx", sheet = "CPC_2023")
 
 
 cpc <- data %>% select(cpc_range =`CPC (Faixa)`, modality = `Modalidade de Ensino`)
 
-cpc <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
-  filter(cpc$cpc_range %in% c("3"))
+cpc_low <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
+  filter(cpc$cpc_range %in% c("1", "2", "3", "4", "5"))
 
-tab <- cpc %>% select(cpc_range, modality) %>% table()
+cpc_high <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
+  filter(cpc$cpc_range %in% c("3", "4", "5"))
+
+tab <- cpc_low %>% select(cpc_range, modality) %>% table()
+
+
+cpc_join <- cpc %>%
+  mutate(cpc_range = as.double(cpc_range)) %>% # Transform cpc_range from char to float, everything that is not numeric will become NA
+  filter(!is.na(cpc_range)) %>% # filtering out anything that is NA
+  mutate(cpc_range = purrr::map_dbl(cpc_range, ~ ifelse(.x == 1, 2, .x))) 
+
+tab_join <- cpc_join %>% select(cpc_range, modality) %>% table()
+
+chisq_test <- tab_join %>% chisq.test(correct = FALSE)
+
+
 
 prop_tab <- tab %>% prop.table(margin = 2)
+
+tab
 
 cpc_frame <- data.frame(prop_tab)
 
@@ -26,12 +44,6 @@ cpc_frame %>% ggplot(mapping = aes(x = cpc_range, fill = modality, y = Freq)) +
 
 chisq_test <- tab %>% chisq.test(correct = FALSE)
 
-# Create contingency table: rows = modality, columns = score category
-table_data <- table(data_categorized$`Modalidade de Ensino`, data_categorized$score_category)
-
-chisq_test <- table_data %>% chisq.test(correct = FALSE)
-
-prop.table(table_data, margin = 1)  # proportions by row (modality)
-
-
+chisq_test$expected
+chisq.test(tab, simulate.p.value = TRUE, B = 10000)
 
