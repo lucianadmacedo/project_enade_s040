20683dd08473b056bc454eab5ebfb010fbbf39c9
Livia Fares <livia.fares97@gmail.com>
2025-09-18 13:06:16 -0400

    Create new t-test; rename variables; add comment documentation

--- CPC_range_modality.R
+++ CPC_range_modality.R
@@ -5,45 +5,48 @@ library(dplyr)
 library(ggplot2)
 library(purrr)
 
-data <- read_excel("~/Downloads/CPC_2023.xlsx", sheet = "CPC_2023")
+data <- read_excel("CPC_2023.xlsx", sheet = "CPC_2023")
 
-
+# Select only modality and CPC range columns
 cpc <- data %>% select(cpc_range =`CPC (Faixa)`, modality = `Modalidade de Ensino`)
 
-cpc_low <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
-  filter(cpc$cpc_range %in% c("1", "2", "3", "4", "5"))
+cpc <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
+  filter(cpc$cpc_range %in% c("1", "2", "3", "4", "5")) %>%
+  mutate(modality = case_when(
+    modality == "Educação a Distância" ~ "Online",
+    modality == "Educação Presencial" ~ "In-Person"
+  ))
 
-cpc_high <- cpc %>% filter(modality %in% c("Educação a Distância", "Educação Presencial")) %>%
-  filter(cpc$cpc_range %in% c("3", "4", "5"))
-
-tab <- cpc_low %>% select(cpc_range, modality) %>% table()
-
-
-cpc_join <- cpc %>%
-  mutate(cpc_range = as.double(cpc_range)) %>% # Transform cpc_range from char to float, everything that is not numeric will become NA
-  filter(!is.na(cpc_range)) %>% # filtering out anything that is NA
-  mutate(cpc_range = purrr::map_dbl(cpc_range, ~ ifelse(.x == 1, 2, .x))) 
-
-tab_join <- cpc_join %>% select(cpc_range, modality) %>% table()
-
-chisq_test <- tab_join %>% chisq.test(correct = FALSE)
-
-
-
-prop_tab <- tab %>% prop.table(margin = 2)
-
+tab <- cpc %>% select(cpc_range, modality) %>% table()
 tab
 
-cpc_frame <- data.frame(prop_tab)
+# Joining 1 and 2 CPC range since there is no online course with CPC 1
+cpc_join <- cpc %>%
+  filter(cpc_range %in% c("1", "2", "3", "4", "5")) %>% 
+  
+  # Recode the character strings directly
+  mutate(cpc_range = case_when(
+    cpc_range %in% c("1", "2") ~ "1 and 2",
+    TRUE ~ cpc_range 
+  ))
 
+# Shows values from joint dataset in one table
+tab_join <- cpc_join %>% select(cpc_range, modality) %>% table()
+tab_join
+
+prop_tab <- tab_join %>% prop.table(margin = 2)
+prop_tab
+
+# Plotting percent of courses/universities by CPC range (x-axis) and modality (color)
+cpc_frame <- data.frame(prop_tab)
 cpc_frame %>% ggplot(mapping = aes(x = cpc_range, fill = modality, y = Freq)) +
   geom_col(position = 'dodge') +
   scale_y_continuous(labels = scales::percent_format()) +
   labs (x = "CPC range" , y = "Percent", fill = '')
 
+# Chi-squared test and t-test
+chisq_test <- tab_join %>% chisq.test(correct = FALSE)
+# t.test(tab_join, var.equal = TRUE) # This does not work because t.test is aggregating the results
 
-chisq_test <- tab %>% chisq.test(correct = FALSE)
-
-chisq_test$expected
-chisq.test(tab, simulate.p.value = TRUE, B = 10000)
+t.test(cpc_range ~ modality, data = cpc_join, var.equal = TRUE)
 
